77a10fc33d6929fdb6894256a6b95ea9f4e712bf
AlexGru <grusch.alex@gmail.com>
2014-05-05 16:42:09 +0200

    added camera preview, aspect ratio not correct yet

--- App/src/com/example/App/MainActivity.java
+++ App/src/com/example/App/MainActivity.java
@@ -1,15 +1,57 @@
 package com.example.App;
 
 import android.app.Activity;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+    final static String TAG = "Gru-APP";
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+    }
+
+    @Override
+    protected void onPause() {
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
+        super.onPause();
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+
+    public void onClick(View view) {
+        Toast.makeText(getApplicationContext(), "Click!", Toast.LENGTH_LONG).show();
     }
 }
