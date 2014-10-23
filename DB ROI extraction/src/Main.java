5158821a7a1476828a45f68366fa2cbacfddd980
Alex Gru <agruschi@cosy.sbg.ac.at>
2014-10-23 21:15:50 +0200

    changed "auflicht" parameters, pretty good working now

--- DB ROI extraction/src/Main.java
+++ DB ROI extraction/src/Main.java
@@ -9,9 +9,10 @@ import java.util.List;
 
 public class Main {
 
-    private static final String inFilesDir = "images/Tag der offenen Tuer - Backup/";
+    //    private static final String inFilesDir = "E:\\Bak\\processing folder ROI\\";
+    private static final String inFilesDir = "E:\\Bak\\processing folder ROI\\auflicht old\\";
     private static final String houghCirclesResultDir = inFilesDir + "houghcircles/";
-    private static final String roiResultDir = inFilesDir + "roi/";
+    private static final String roiResultDir = houghCirclesResultDir + "roi/";
 
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
@@ -26,68 +27,127 @@ public class Main {
                 return name.toLowerCase().endsWith(".jpg");
             }
         };
+
+        Mat inImageGray = null;
+        Mat inImage = null;
+        Mat inImagePaintable = null;
+        Mat circles;
+        Mat roi = null;
+        Point posCircle;
+        Point centerOfFocusArea;
+        File circlesFile;
+        File roiRectangleFile;
+        File roiFile;
+        List<Point> circlePoints;
+        double[] currentCircleCoords;
+        String circlesFileName;
+        Point start;
+        Point end;
+        String currFileInName;
+        String roiRectangleFileName;
+        String roiFileName;
+
         for (final File inFile : imageDir.listFiles(fileNameFilter)) {
-            String currFileInName = inFile.getName();
+            currFileInName = inFile.getName();
             System.out.println(currFileInName);
 
-            Mat inImageGray = Highgui.imread(inFile.toString(), Highgui.CV_LOAD_IMAGE_GRAYSCALE);
-            Mat inImage = Highgui.imread(inFile.toString());
+            inImageGray = Highgui.imread(inFile.toString(), Highgui.CV_LOAD_IMAGE_GRAYSCALE);
+            inImage = Highgui.imread(inFile.toString());
 
-            Mat inImagePaintable = new Mat(inImage.rows(), inImage.cols(), inImage.type());
+            inImagePaintable = new Mat(inImage.rows(), inImage.cols(), inImage.type());
             inImage.copyTo(inImagePaintable);
 
             System.out.println("inImageGray size: " + inImageGray.size());
 
-            Mat circles = new Mat(inImageGray.rows(), inImageGray.cols(), inImageGray.type());
-            Imgproc.HoughCircles(inImageGray, circles, Imgproc.CV_HOUGH_GRADIENT, 1, 50, 30, 30, 30, 50);
+            circles = new Mat(inImageGray.rows(), inImageGray.cols(), inImageGray.type());
+
+            //auflicht settings
+            double canny1 = 10;
+            double canny2 = 10;
+            double dp = 1;
+            double minDistance = 300;
+            int minRadius = 20;
+            int maxRadius = 60;
+
+            //special
+//            double canny1 = 15;
+//            double canny2 = 10;
+//            double dp = 1;
+//            double minDistance = 150;
+//            int minRadius = 20;
+//            int maxRadius = 50;
+//            Imgproc.Canny(inImageGray, inImageGray, canny1, canny2);
+//            Imgproc.GaussianBlur(inImageGray,inImageGray,new Size(3,3),3);
+            //end special
+
+
+            Imgproc.HoughCircles(inImageGray, circles, Imgproc.CV_HOUGH_GRADIENT, dp, minDistance, canny1, canny2, minRadius, maxRadius);
             System.out.println("#circles: " + circles.cols());
             System.out.println("size: " + circles.size());
 
-            List<Point> circlePoints = new ArrayList<Point>();
+            circlePoints = new ArrayList<Point>();
             if (circles.cols() > 0) {
                 for (int i = 0; i < circles.cols(); i++) {
-                    double[] current = circles.get(0, i);
-                    if (current == null) {
+                    currentCircleCoords = circles.get(0, i);
+                    if (currentCircleCoords == null) {
                         continue;
                     }
-                    Point posCircle = new Point(current[0], current[1]);
-                    Point centerInImage = new Point(inImageGray.cols() / 2, inImageGray.rows() / 2);
-                    final int MAX_DIFF = 200;
-                    if (posCircle.x < centerInImage.x + MAX_DIFF
-                            && posCircle.x > centerInImage.x - MAX_DIFF
-                            && posCircle.y < centerInImage.y + MAX_DIFF
-                            && posCircle.y > centerInImage.y - MAX_DIFF) {
+                    posCircle = new Point(currentCircleCoords[0], currentCircleCoords[1]);
+
+                    int centerOfFocusArea_X = 2155;
+                    int centerOfFocusArea_Y = 1100;
+                    centerOfFocusArea = new Point(centerOfFocusArea_X, centerOfFocusArea_Y);
+
+                    final int MAX_DIFF_X = 400;
+                    final int MAX_DIFF_Y = 100;
+                    if (posCircle.x < centerOfFocusArea.x + MAX_DIFF_X
+                            && posCircle.x > centerOfFocusArea.x - MAX_DIFF_X
+                            && posCircle.y < centerOfFocusArea.y + MAX_DIFF_Y
+                            && posCircle.y > centerOfFocusArea.y - MAX_DIFF_Y
+//                            && posCircle.x > centerOfFocusArea_X - 300
+                            ) {
                         circlePoints.add(posCircle);
-                        Core.circle(inImagePaintable, posCircle, (int) current[2], new Scalar(0, 255, 0), 20);
+                        Core.circle(inImagePaintable, posCircle, (int) currentCircleCoords[2], new Scalar(0, 255, 0), 20);
                     }
                 }
 
-                String circlesFileName = currFileInName.replace(".", "_houghCircles.");
-                File circlesFile = new File(houghCirclesResultDir + circlesFileName);
-                Highgui.imwrite(circlesFile.toString(), inImagePaintable);
+                circlesFileName = currFileInName.replace(".", "_houghCircles.");
 
                 if (circlePoints.size() != 2) {
                     System.err.println("Did not find the expected number of circles: " + circlePoints.size());
+                    circlesFile = new File(houghCirclesResultDir + "failed/" + circlesFileName);
+                    Highgui.imwrite(circlesFile.toString(), inImagePaintable);
                 } else {
-                    String roiRectangleFileName = currFileInName.replace(".", "_roiRectangle.");
-                    File roiRectangleFile = new File(roiResultDir + roiRectangleFileName);
-                    String roiFileName = currFileInName.replace(".", "_roi.");
-                    File roiFile = new File(roiResultDir + roiFileName);
+                    circlesFile = new File(houghCirclesResultDir + circlesFileName);
+                    Highgui.imwrite(circlesFile.toString(), inImagePaintable);
 
-                    Point start;
+                    roiRectangleFileName = currFileInName.replace(".", "_roiRectangle.");
+                    roiRectangleFile = new File(roiResultDir + roiRectangleFileName);
+                    roiFileName = currFileInName.replace(".", "_roi.");
+                    roiFile = new File(roiResultDir + roiFileName);
+
                     if (circlePoints.get(0).x < circlePoints.get(1).x) {
                         start = circlePoints.get(0);
                     } else {
                         start = circlePoints.get(1);
                     }
                     start.x -= 100;
-                    start.y += 160;
-                    Point end = new Point(start.x + 500, start.y + 500);
+                    start.y += 300;
+                    end = new Point(start.x + 700, start.y + 700);
                     Core.rectangle(inImagePaintable, start, end, new Scalar(0, 0, 255), 10);
                     Highgui.imwrite(roiRectangleFile.toString(), inImagePaintable);
-                    Mat roi = inImage.submat(new Rect(start, end));
+                    roi = inImage.submat(new Rect(start, end));
                     Highgui.imwrite(roiFile.toString(), roi);
                 }
+            }
+
+            try {
+                inImageGray.release();
+                inImage.release();
+                inImagePaintable.release();
+                circles.release();
+                roi.release();
+            } catch (NullPointerException e) {
             }
         }
     }
